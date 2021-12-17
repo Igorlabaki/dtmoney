a55c0f2500cd8f99024ed0e5dda04a593e6b0932
Igor Goncalo <igorlabakig@gmail.com>
2021-12-17 17:26:03 +0000

    DTmoney

---
+++ src/components/NewTransactionModal/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.form`
+   
+   h2{
+      color: var(--text-title);
+      font-size: 1.5rem;
+      margin-bottom: 2rem;
+   }
+
+   input{
+      width: 20rem;
+      padding: 0% 1.5rem;
+      height: 4rem;
+      border-radius: 0.25rem;
+      background: #e7e9ee;
+      border: 1px solid #d7d7d7;
+      font-weight: 400;
+      font-size: 1rem;
+   }
+`;
---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2rem;
+    margin-top: -10rem;
+
+    div{
+        background: var(--shape);
+        padding: 1.5rem 2rem;
+        border-radius: .25rem;
+        color: var(--text-title);
+        strong{
+            display: block;
+            margin-top: 1rem;
+            font-size: 2rem;
+            font-weight: 500;
+            line-height: 3rem;
+        }
+        
+        &.highligth-background{
+            background: var(--green);
+            color: #FFF;
+        }
+    }
+
+
+`;
