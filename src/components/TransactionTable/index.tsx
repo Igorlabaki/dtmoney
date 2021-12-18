cb32a803c4933a6f0b7c91b03b244c99f024684a
Igor Goncalo <igorlabakig@gmail.com>
2021-12-18 17:31:59 +0000

    dds

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -17,9 +17,10 @@ export  function TransactionTable() {
 
     useEffect(() => {
        api.get('transactions')
-       .then(response => setTransactions(response.data.transactions))
-       
-    }, []);
+       .then(response => {
+        setTransactions(response.data.transactions)
+       })
+    }, [transactions]);
 
     return (
       <Container>
@@ -34,12 +35,12 @@ export  function TransactionTable() {
             </thead>
             <tbody>
                {transactions.map(transaction => {
-                   <tr key={transaction.id}>
-                    <td>{transaction.title}</td>
-                    <td className={transaction.type}>{transaction.amount}</td>
-                    <td>{transaction.category}</td>
-                   <td>{transaction.createdAt}</td>
-               </tr>
+                    <tr key={transaction.id}>
+                        <td>{transaction.title}</td>
+                        <td className={transaction.type}>{transaction.amount}</td>
+                        <td>{transaction.category}</td>
+                        <td>{transaction.createdAt}</td>
+                    </tr>
                })}
             </tbody>
           </table>
